78cd02adfe540a2934d6df0658e25dce8c9de4e8
Imran Sarkar (Sabbir) <107399210+Imran-Sarkar-Sabbir@users.noreply.github.com>
2023-07-23 10:58:19 +0600

    fix unresolved reference issue.

--- android/src/main/kotlin/com/pravera/flutter_foreground_task/utils/ForegroundServiceUtils.kt
+++ android/src/main/kotlin/com/pravera/flutter_foreground_task/utils/ForegroundServiceUtils.kt
@@ -11,7 +11,6 @@ import android.os.Build
 import android.os.PowerManager
 import android.provider.Settings
 import android.view.WindowManager
-import com.pravera.flutter_foreground_task.utils.CallActivity
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 
